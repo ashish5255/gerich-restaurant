77165dc7e1e427e28b6f424998bf21132f104145
Ashish <ashishadhikari5255@gmail.com>
2024-09-16 17:28:26 +0545

    Add assets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from "react";
 
+import {
+  AboutUs,
+  Chef,
+  FindUs,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  SpecialMenu,
+} from "./container";
+import { Navbar } from "./components";
+import "./App.css";
+
 const App = () => {
   return (
     <div>
-      <h1>Hello</h1>
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <SpecialMenu />
+      <Chef />
+      <Intro />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Footer />
     </div>
   );
 };
